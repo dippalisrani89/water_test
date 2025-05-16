2f48af21a59328e1d739ba0e56c029056dafe96b
dippalisrani89 <81163003+dippalisrani89@users.noreply.github.com>
2025-05-17 03:35:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI (
     description = "Predicting Water Potability "
 )
 
-with open("D:\ml_pipeline\model.pkl","rb") as f:
+with open("model.pkl","rb") as f:
     model = pickle.load(f)
 
 @app.get("/")
